0d8706b06300970d9a97479698d769ec0fcccc3c
baseball1989 <pinball1989@icloud.com>
2022-04-12 15:57:28 +0900

    start

---
+++ rbonus/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <RootPage />
+</template>
+
+<script>
+import RootPage from './components/RootPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    RootPage
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
